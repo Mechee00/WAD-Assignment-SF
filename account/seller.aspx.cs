19d185c5a7c8c4131071427e6e888614a466600e
Cheah Pin Chee <mr.chee37@gmail.com>
2022-09-26 20:52:20 +0800

    Added Block customer from accessing seller page

--- account/seller.aspx.cs
+++ account/seller.aspx.cs
@@ -6,7 +6,17 @@ namespace WAD_Assignment_SF.account
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
 
+                user usr = (user)Session["user"];
+                if (usr != null)
+                {
+                    if (usr.type == "customer")
+                    {
+                        Response.Redirect("../home.aspx");
+                    }
+                }
         }
     }
 }
